d7054895c5d2d75d2bde7c61059f0c254b0b9beb
DoggieLicc <76987398+DoggieLicc@users.noreply.github.com>
2021-01-12 17:08:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
   name='Discord Webhooks',
   version='1.0.5',
   py_modules=['discord_webhooks'],
-  url='https://github.com/JamesIves/discord-webhooks',
-  author='James Ives',
-  author_email='iam@jamesiv.es',
+  url='https://github.com/DoggieLicc/discord-webhooks',
+  author='Doggie Licc',
+  author_email='doggietoe@gmail.com',
   description='Easy to use module for Python which allows for sending of webhooks to a Discord server.',
   license='MIT',
   install_requires=[
